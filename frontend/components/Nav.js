4fe272eb1a40bf7657a18fb0ed4f84244be041e9
Nenad Radovanovic <nradovan@calliduscloud.com>
2018-11-19 15:03:59 +0100

    styling Header, Nav and Page

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,14 +1,24 @@
 import Link from 'next/link'
+import NavStyles from './styles/NavStyles'
 
 const Nav = () => (
-  <div>
-    <Link href='/'>
-      Home
+  <NavStyles>
+    <Link href="/items">
+      <a>Items</a>
     </Link>
-    <Link href='/sell'>
-      Sell!
+    <Link href="/sell">
+      <a>Sell!</a>
     </Link>
-  </div>
+    <Link href="/signup">
+      <a>Signup</a>
+    </Link>
+    <Link href="/orders">
+      <a>Orders</a>
+    </Link>
+    <Link href="/me">
+      <a>Account</a>
+    </Link>
+  </NavStyles>
 )
 
 export default Nav
