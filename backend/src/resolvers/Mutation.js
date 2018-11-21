54eda1c7a11725a7404e1aaf6f1e620226a85892
Nenad Radovanovic <nradovan@calliduscloud.com>
2018-11-21 14:06:00 +0100

    testing out data flow from mutation to query

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,10 @@
-const mutations = {};
+const mutations = {
+  createDog(parent, args, ctx, info) {
+    global.dogs = global.dogs || []
+    const newDog = { name: args.name }
+    global.dogs.push(newDog)
+    return newDog
+  }
+};
 
 module.exports = mutations;
