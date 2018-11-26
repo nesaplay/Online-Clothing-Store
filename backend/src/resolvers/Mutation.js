b07a32e688f2d4b3e821a40a56e51caac9800abc
Nenad Radovanovic <nradovan@calliduscloud.com>
2018-11-26 15:29:04 +0100

    creating first mutation and getting all items

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,10 +1,11 @@
-const mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || []
-    const newDog = { name: args.name }
-    global.dogs.push(newDog)
-    return newDog
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem({
+      data: { ...args }
+    }, info);
+
+    return item
   }
 };
 
-module.exports = mutations;
+module.exports = Mutations;
