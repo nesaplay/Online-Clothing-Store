56dd96851bc9b4e86123a09d7213e3dc7ce8aca7
Nenad Radovanovic <nradovan@calliduscloud.com>
2018-12-18 15:47:17 +0100

    signup mutation and schema updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,11 @@
-require('dotenv').config({ path: 'variables.env' })
-const createServer = require('./createServer')
-const db = require('./db')
+const cookieParser = require('cookie-parser');
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
 
-const server = createServer()
+const server = createServer();
+
+server.express.use(cookieParser());
 
 server.start({
   cors: {
