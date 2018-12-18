8a4b0489cfaed764a2a3d51000c479abf6b853da
Nenad Radovanovic <nradovan@calliduscloud.com>
2018-12-18 12:15:08 +0100

    user connection added as a query

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -3,6 +3,7 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
+  itemsConnection: forwardTo('db'),
   // async items(parent, args, ctx, info) {
   //   const items = await ctx.db.query.items();
   //   return items;
