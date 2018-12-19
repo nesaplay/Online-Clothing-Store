348e6da697d3a4b5723ea6a1da425aa8b60171d0
Nenad Radovanovic <nradovan@calliduscloud.com>
2018-12-19 15:25:29 +0100

    added new query to get logged in user

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -4,10 +4,13 @@ const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
   itemsConnection: forwardTo('db'),
-  // async items(parent, args, ctx, info) {
-  //   const items = await ctx.db.query.items();
-  //   return items;
-  // }
+  me(parent, args, ctx, info) {
+    if (!ctx.request.userId) return null;
+
+    return ctx.db.query.user({
+      where: { id: ctx.request.userId }
+    }, info);
+  }
 };
 
 module.exports = Query;
