7411b0388f63e0b25fd1640895295769973eb617
Nenad Radovanovic <nradovan@calliduscloud.com>
2018-12-19 15:25:10 +0100

    getting logged in user in session

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -4,10 +4,11 @@ import gql from 'graphql-tag';
 import styled from 'styled-components'
 import Item from './Item';
 import Pagination from './Pagination';
+import { perPage } from '../config';
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -34,8 +35,8 @@ class Items extends Component {
   render () {
     return (
       <Center>
-        <Pagination page={this.props.page} />
-        <Query query={ALL_ITEMS_QUERY}>
+        <Pagination page={this.props.page} /> 
+        <Query query={ALL_ITEMS_QUERY} variables={{ skip: this.props.page * perPage - perPage }}>
           {({ data, loading, error }) => {
             if (loading) return <p>loading...</p>
             if (error) return <p>Error: {error.message}</p>
---
+++ frontend/pages/signup.js
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import Signup from '../components/Signup';
+import styled from 'styled-components';
+
+const Columns = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
+`;
+
+const SignupPage = props => (
+  <Columns>
+    <Signup />    
+    <Signup />    
+    <Signup />    
+  </Columns>
+)
+
+export default SignupPage;
