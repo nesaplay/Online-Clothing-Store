98be6a0417a6c7f1e797cf3ce2d7937c65f9d0b3
Nenad Radovanovic <nradovan@calliduscloud.com>
2018-12-26 10:06:25 +0100

    auth wrapper

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -6,8 +6,16 @@ const { transport, makeANiceEmail } = require('../mail');
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in to do that!');
+    }
     const item = await ctx.db.mutation.createItem({
-      data: { ...args }
+      data: {
+        user: {
+          connect: { id: ctx.request.userId }
+        },
+        ...args
+      }
     }, info);
 
     return item
--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,9 +1,12 @@
 import Link from 'next/link'
 import CreateItem from '../components/CreateItem';
+import PleaseSignIn from '../components/PleaseSignin';
 
 const Sell = props => (
   <div>
-    <CreateItem />
+    <PleaseSignIn>
+      <CreateItem />
+    </PleaseSignIn>
   </div>
 )
 
